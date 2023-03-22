fd65279e0f2ddcb8872ea84e29780f05bff9cf4c
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2023-03-22 16:45:44 +0100

    Add files via upload

---
+++ labs/basic-resource-ec2-ubuntu/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+	region = "eu-central-1"
+}
+
+resource "aws_instance" "instance" {
+	ami           = "ami-0d1ddd83282187d18" # Ubuntu 22.04 eu-central-1 Frankfurt
+	instance_type = "t2.nano"
+
+	tags = {
+		Name = "Basic Instance"
+	}
+}
