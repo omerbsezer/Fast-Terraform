118416a8804f4b94038532bde46b23c77fbb0e7f
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2023-03-22 16:47:09 +0100

    Add files via upload

---
+++ labs/terraform-docker-without-cloud/main.tf
@@ -0,0 +1,30 @@
+# windows, prerequisite: install docker on your system
+# ref: https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+# terraform init, apply, docker ps, destroy
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
