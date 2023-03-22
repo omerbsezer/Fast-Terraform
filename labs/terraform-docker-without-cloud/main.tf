79ed97268f9d1a9a28198a6acfea5ceb465d0da1
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2023-03-23 00:10:35 +0100

    Update main.tf

--- labs/terraform-docker-without-cloud/main.tf
+++ labs/terraform-docker-without-cloud/main.tf
@@ -1,30 +1,41 @@
 # windows, prerequisite: install docker on your system
-# ref: https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
-# terraform init, apply, docker ps, destroy
+# details, usage: https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "~> 3.0.2"
     }
   }
 }
 
 provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+  host    =  "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
+resource "docker_image" "windows" {
+  name         = "mcr.microsoft.com/powershell:lts-windowsservercore-1809"
+  keep_locally = true
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
+# docker container run -p 80:8000 --name=tutorial -dit mcr.microsoft.com/powershell:lts-windowsservercore-1809 powershell
+resource "docker_container" "windows" {
+  image = docker_image.windows.image_id
   name  = "tutorial"
-
+  
+  stdin_open = true             # docker run -i
+  tty        = true             # docker run -t
+  
+  attach     = true             # docker run -d
+  entrypoint = ["powershell"]
+  
   ports {
     internal = 80
     external = 8000
   }
 }
+
+#  docker container ls -a
+#  docker container exec -it tutorial powershell
+#  ls, exit
+#  terraform destroy -auto-approve
