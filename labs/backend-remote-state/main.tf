a59b70d315d9c5a65378186f69ebac2aa5ac1c86
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2023-04-07 19:14:09 +0200

    Add files via upload

---
+++ labs/backend-remote-state/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key = "key/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+	region = "eu-central-1"
+}
+
+resource "aws_instance" "instance" {
+	ami           = "ami-0d1ddd83282187d18" # Ubuntu 22.04 eu-central-1 Frankfurt
+	instance_type = "t2.nano"
+
+	tags = {
+		Name = "Basic Instance"
+	}
+}
