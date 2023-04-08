7282ed1f2b25c5b53b19d36f6cf173e65cc440fe
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2023-04-08 14:58:08 +0200

    Add files via upload

--- samples/lambda-role-policy-apigateway-python/api-gateway.tf
+++ samples/lambda-role-policy-apigateway-python/api-gateway.tf
@@ -1,6 +1,7 @@
+# Create API Gateway with Rest API type
 resource "aws_api_gateway_rest_api" "example" {
-  name        = "ServerlessFunction"
-  description = "Terraform Serverless Application"
+  name        = "Serverless"
+  description = "Serverless Application using Terraform"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
@@ -16,15 +17,18 @@ resource "aws_api_gateway_method" "proxy" {
    authorization = "NONE"
 }
 
+# API Gateway - Lambda Connection
 resource "aws_api_gateway_integration" "lambda" {
    rest_api_id = aws_api_gateway_rest_api.example.id
    resource_id = aws_api_gateway_method.proxy.resource_id
    http_method = aws_api_gateway_method.proxy.http_method
    integration_http_method = "POST"
-   type                    = "AWS_PROXY" # With AWS_PROXY, it causes API gateway to call into the API of another AWS service
+   type                    = "AWS_PROXY"  # With AWS_PROXY, it causes API gateway to call into the API of another AWS service
    uri                     = aws_lambda_function.lambda_function.invoke_arn
 }
 
+# The proxy resource cannot match an empty path at the root of the API. 
+# To handle that, a similar configuration must be applied to the root resource that is built in to the REST API object
 resource "aws_api_gateway_method" "proxy_root" {
    rest_api_id   = aws_api_gateway_rest_api.example.id
    resource_id   = aws_api_gateway_rest_api.example.root_resource_id
@@ -37,10 +41,11 @@ resource "aws_api_gateway_integration" "lambda_root" {
    resource_id = aws_api_gateway_method.proxy_root.resource_id
    http_method = aws_api_gateway_method.proxy_root.http_method
    integration_http_method = "POST"
-   type                    = "AWS_PROXY"
+   type                    = "AWS_PROXY"  # With AWS_PROXY, it causes API gateway to call into the API of another AWS service
    uri                     = aws_lambda_function.lambda_function.invoke_arn
 }
 
+# Deploy API Gateway
 resource "aws_api_gateway_deployment" "example" {
    depends_on = [
      aws_api_gateway_integration.lambda,
@@ -50,6 +55,7 @@ resource "aws_api_gateway_deployment" "example" {
    stage_name  = "test"
 }
 
+# Output to the URL 
 output "base_url" {
   value = aws_api_gateway_deployment.example.invoke_url
 }
