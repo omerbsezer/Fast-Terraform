4d668c0817dbe38b92f879a543bda6af3ec73ad0
Ömer Berat Sezer <omer.b.sezer@gmail.com>
2024-10-04 00:24:32 +0200

    Update api-gateway.tf

--- samples/lambda-role-policy-apigateway-python/api-gateway.tf
+++ samples/lambda-role-policy-apigateway-python/api-gateway.tf
@@ -4,12 +4,14 @@ resource "aws_api_gateway_rest_api" "example" {
   description = "Serverless Application using Terraform"
 }
 
+# Defines a resource in the API Gateway that will capture any request path. The path_part = "{proxy+}" allows API Gateway to match all requests that have any path pattern, enabling dynamic routing.
 resource "aws_api_gateway_resource" "proxy" {
    rest_api_id = aws_api_gateway_rest_api.example.id
    parent_id   = aws_api_gateway_rest_api.example.root_resource_id
    path_part   = "{proxy+}"     # with proxy, this resource will match any request path
 }
 
+# Configures to allow any HTTP method (GET, POST, DELETE, etc.) and does not require any specific authorization. It's set for the previously defined proxy resource.
 resource "aws_api_gateway_method" "proxy" {
    rest_api_id   = aws_api_gateway_rest_api.example.id
    resource_id   = aws_api_gateway_resource.proxy.id
@@ -18,6 +20,7 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 # API Gateway - Lambda Connection
+# The AWS_PROXY type means API Gateway will directly pass the request details (method, headers, body, path parameters, etc.) to the Lambda function
 resource "aws_api_gateway_integration" "lambda" {
    rest_api_id = aws_api_gateway_rest_api.example.id
    resource_id = aws_api_gateway_method.proxy.resource_id
@@ -27,8 +30,9 @@ resource "aws_api_gateway_integration" "lambda" {
    uri                     = aws_lambda_function.lambda_function.invoke_arn
 }
 
-# The proxy resource cannot match an empty path at the root of the API. 
-# To handle that, a similar configuration must be applied to the root resource that is built in to the REST API object
+# The proxy resource cannot match an empty path at the root of the API. API GW doesn't route requests to the root (/) path using the proxy, so a separate method and integration for the root resource are required.
+# To handle that, a similar configuration must be applied to the root resource that is built in to the REST API object.
+# This ensures that requests to the root (e.g., https://api.example.com/) are also forwarded to the Lambda function.
 resource "aws_api_gateway_method" "proxy_root" {
    rest_api_id   = aws_api_gateway_rest_api.example.id
    resource_id   = aws_api_gateway_rest_api.example.root_resource_id
@@ -46,6 +50,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
 }
 
 # Deploy API Gateway
+# Deploys the API to the specified stage (test stage). The depends_on ensures that the API is not deployed until both the Lambda integrations (for proxy and root) are complete.
 resource "aws_api_gateway_deployment" "example" {
    depends_on = [
      aws_api_gateway_integration.lambda,
